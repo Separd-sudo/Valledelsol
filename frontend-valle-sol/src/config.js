daecf9f5b50b9964ca808098147986cbc1793b82
Separd <brandon.sepulveda14@gmail.com>
2026-07-07 14:57:03 -0400

    Fix:Reportes aplicacion habilitando KAFKA

--- frontend-valle-sol/src/config.js
+++ frontend-valle-sol/src/config.js
@@ -1,6 +1,6 @@
 // Centraliza las URLs de los servicios para facilitar su mantenimiento y actualización.
 // Cambiamos 'import.meta.env' por 'process.env' y agregamos el prefijo NEXT_PUBLIC_ estándar de Next.js
-export const BFF_BASE_URL = "http://localhost:8080/api/v1/bff";
+export const BFF_BASE_URL = "http://192.168.64.1:8080/api/v1/bff";
 
 
 // OJO: Usamos comillas invertidas (backticks ` ) para inyectar la variable correctamente
